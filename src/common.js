f87301dfe0f2f88eb0373b611191817d6b8e20b7
Vage Petrosian <Vage_Petrosian@epam.com>
2018-11-20 22:18:00 +0300

    correct application build

--- src/common.js
+++ src/common.js
@@ -1,10 +1,12 @@
 "use strict";
 
+import '@babel/polyfill';
+import 'whatwg-fetch';
+
 const newsApiKey = 'cf4cb85ba9f447f6aa4c35febe6d5837';
 const newsApiUrl = 'https://newsapi.org/v2/';
 
-import 'promise-polyfill/src/polyfill';
-import 'whatwg-fetch';
+document.onload = SetupPage();
 
 function GetRequestHeaders() {
   let requestHeaders = new Headers();
