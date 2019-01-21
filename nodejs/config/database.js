0303f4d27d141dec908e197baaabfc7473b81729
Vage Petrosian <Vage_Petrosian@epam.com>
2019-01-21 19:09:33 +0300

    removed nodejs project from this repo

--- nodejs/config/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/news';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-module.exports = mongoose;
--- nodejs/routes/news.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const newsController = require('../controllers/news');
-router.get('/:id?', newsController.get);
-router.post('/', newsController.add);
-router.put('/:id?', newsController.update);
-router.delete('/:id?', newsController.delete);
-module.exports = router;
